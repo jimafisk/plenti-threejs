98cd39b4b4413948a4c7a0363d5045e522f292a6
jimafisk <jimafisk@gmail.com>
2021-12-01 08:57:09 -0500

    Remove unused bundlers.

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    //input: 'src/main.js',
-    //input: ['three'],
-    input: './node_modules/three/src/Three.js',
-    output: {
-      file: 'assets/bundle.js',
-      format: 'esm'
-    }
-};
